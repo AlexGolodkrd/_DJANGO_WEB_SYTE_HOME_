77966f96c0a5e2699dd3f1c465d2765232a3f436
AlexGolodkrd <aleks.golodny@gmail.com>
2025-09-08 12:39:46 +0300

    registered simple tag for categories + add id goods + add defs to models

--- app/goods/models.py
+++ app/goods/models.py
@@ -29,15 +29,18 @@ class Products(models.Model):
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
 
-
-
-
-
-    def __str__(self):
-        return self.name
-    
-
     class Meta:
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
+    
+    def display_id(self):
+        return f'{self.id:05}'
+    
+    def sell_price(self):
+        if self.discount:
+            return round(self.price * (1 - self.discount / 100), 2)
+        return self.price
